21a64de64832ea38fb0fe7fd99757044ae406916
Justin Byrd <justinbyrd7@gmail.com>
2023-11-01 17:00:18 -0500

    figuring out routes in v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Login = () => {
+  return <h2>Login</h2>;
+};
+const FriendsList = () => {
+  return <h2>FriendsList</h2>;
+};
+const AddFriends = () => {
+  return <h2>AddFriends</h2>;
+};
 
 function App() {
   return (
     <div className="App">
       <h2>Client Auth Project</h2>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/friends" element={<FriendsList />} />
+        <Route path="/friends/add" element={<AddFriends />} />
+        <Route path="/logout" element={<Login />} />
+      </Routes>
     </div>
   );
 }
